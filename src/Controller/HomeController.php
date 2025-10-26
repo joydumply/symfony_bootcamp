50401e4c2ff1d61e2ef37d03458e4b0fa7998919
joydumply <nick.babichev@gmail.com>
2025-10-26 15:12:51 +0100

    Partials & include in templates

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -7,11 +7,14 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
 
-class HomeController extends AbstractController {
-    #[Route( '/hello/{name}' )]
-    public function index( $name ): Response {
+class HomeController extends AbstractController
+{
+    #[Route('/', name: 'app_home'),]
+    public function index($name = "Nik"): Response
+    {
         return $this->render(
-            'home/index.html.twig', [
+            'home/index.html.twig',
+            [
                 'name' => $name,
             ]
         );
